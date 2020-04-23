cefe0d43c60576a39fad6888a1e1abab95e60423
ethanharman12 <ethanharman12@gmail.com>
2020-04-22 23:47:20 -0400

    Enabled Touch

--- wwwroot/js/PuzzlePlay/ActiveGame.js
+++ wwwroot/js/PuzzlePlay/ActiveGame.js
@@ -93,9 +93,32 @@ function startGame() {
         });
 
         gameArea.start();
-        gameArea.canvas.onmousedown = startMoving;
-        gameArea.canvas.onmousemove = movePiece;
-        gameArea.canvas.onmouseup = stopMoving;
+        var canvas = gameArea.canvas;
+        canvas.onmousedown = startMoving;
+        canvas.onmousemove = movePiece;
+        canvas.onmouseup = stopMoving;
+
+        // Set up touch events for mobile, etc
+        canvas.addEventListener("touchstart", function (e) {
+            e.preventDefault();
+            displayDebugParameters(e);
+            //var mouseEvent = new MouseEvent("mousedown", getTouchPos(e));
+            //gameArea.canvas.dispatchEvent(mouseEvent);
+            startMoving(getTouchPos(e));
+        }, false);
+        canvas.addEventListener("touchend", function (e) {
+            e.preventDefault();
+            //var mouseEvent = new MouseEvent("mouseup", getTouchPos(e));
+            //gameArea.canvas.dispatchEvent(mouseEvent);
+            stopMoving();
+        }, false);
+        canvas.addEventListener("touchmove", function (e) {
+            e.preventDefault();
+            displayDebugParameters(e);
+            //var mouseEvent = new MouseEvent("mousemove", getTouchPos(e));
+            //gameArea.canvas.dispatchEvent(mouseEvent);
+            movePiece(getTouchPos(e));
+        }, false);        
     }).catch(function (err) {
         return console.error(err.toString());
     });
@@ -147,10 +170,19 @@ function movePiece(event) {
 function startMoving(event) {    
     currentPiece = puzzleGame.getClickedPiece(event.offsetX, event.offsetY);
 
+    //var touches = [];
+    //touches.push({clientX: 1, pageX: 2, screenX: 3});
+    //displayDebugParameters({targetTouches: touches});
+    
     if (currentPiece) {
+        $("#touchedId").text(currentPiece.row + " " + currentPiece.column);
+
         originalOffsetX = event.offsetX - currentPiece.x;
         originalOffsetY = event.offsetY - currentPiece.y;
-    }    
+    }
+    else {
+        $("#touchedId").text("Miss: " + event.offsetX + " " + event.offsetY);
+    }
 }
 
 function stopMoving(event) {
@@ -189,3 +221,31 @@ $(document).ready(function () {
         startGame();
     }
 });
+
+// Get the position of a touch relative to the canvas
+function getTouchPos(touchEvent) {
+    var rect = touchEvent.target.getBoundingClientRect();
+    var x = touchEvent.targetTouches[0].clientX - rect.left;
+    var y = touchEvent.targetTouches[0].clientY - rect.top;
+
+    return {
+        offsetX: x,
+        offsetY: y
+    };
+}
+
+function displayDebugParameters(touchEvent) {
+
+    //var rect = touchEvent.target.getBoundingClientRect();
+    //var touch = touchEvent.targetTouches[0];
+
+    //$("#touchId").text(touch.identifier);
+    //$("#clientX").text(touch.clientX);
+    //$("#clientY").text(touch.clientY);
+    //$("#pageX").text(touch.pageX);
+    //$("#screenX").text(touch.screenX);
+    //$("#rectLeft").text(rect.left);
+    //$("#rectTop").text(rect.top);
+    //$("#pieceX").text(puzzleGame.puzzle[0][0].x);
+    //$("#pieceY").text(puzzleGame.puzzle[0][0].y);
+}
